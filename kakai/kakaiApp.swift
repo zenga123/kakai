5ce70fb4a6e4c43730dd2257afea14fea37caebf
zenga123 <zenga85@naver.com>
2025-03-16 16:10:25 +0900

    Initial Commit

---
+++ kakai/kakaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kakaiApp.swift
+//  kakai
+//
+//  Created by musung on 2025/03/16.
+//
+
+import SwiftUI
+
+@main
+struct kakaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
